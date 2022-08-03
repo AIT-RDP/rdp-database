63e90f3def9070af5ae3483f8e30c3a93aab4329
Michael Spiegel <michael.spiegel@ait.ac.at>
2022-08-03 18:13:25 +0200

    feat: Enable dblink extension before creating a new DB.

--- e3_ems/env.py
+++ e3_ems/env.py
@@ -80,11 +80,12 @@ def auto_create_db():
             DO
             $do$
             BEGIN
-               IF EXISTS (SELECT FROM pg_database WHERE datname = '{db_name}') THEN
-                  RAISE NOTICE 'Database already exists';
-               ELSE
-                  PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE {db_name}');
-               END IF;
+                IF EXISTS (SELECT FROM pg_database WHERE datname = '{db_name}') THEN
+                    RAISE NOTICE 'Database already exists';
+                ELSE
+                    CREATE EXTENSION IF NOT EXISTS dblink;
+                    PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE {db_name}');
+                END IF;
             END
             $do$;
         """)
