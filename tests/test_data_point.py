46ca1f60ca7d7f5a4e9cb80d886ef11298e5a6d8
Michael Spiegel <michael.spiegel@ait.ac.at>
2025-01-14 16:39:13 +0100

    feat: Drafted the unit test environment (#5)

---
+++ tests/test_data_point.py
@@ -0,0 +1,90 @@
+"""
+Tests the basic data point creation and update logic
+"""
+
+import pandas as pd
+import pytest
+import sqlalchemy.sql as sql
+
+
+def test_direct_dp_creation_minimal(clean_db, sql_engine_data_source):
+    """Tests directly creating a data point by inserting into the table"""
+
+    with sql_engine_data_source.connect() as con:
+        con.execute(sql.text("""
+            INSERT INTO data_points(name, device_id, location_code, data_provider) VALUES
+                ('name_0', 'device_0', 'nowhere special', 'crystal ball'),
+                ('name_1', NULL, 'here', 'common knowledge');
+        """))
+
+        data = pd.read_sql("""
+            SELECT id, name, device_id, location_code, data_provider, unit, view_role, metadata
+                FROM data_points
+                ORDER BY name;
+        """, con)
+
+    assert data is not None
+    assert isinstance(data, pd.DataFrame)
+
+    pd.testing.assert_series_equal(data["name"], pd.Series(["name_0", "name_1"]), check_names=False)
+    pd.testing.assert_series_equal(data["device_id"], pd.Series(["device_0", None]), check_names=False)
+    pd.testing.assert_series_equal(data["location_code"], pd.Series(["nowhere special", "here"]), check_names=False)
+    pd.testing.assert_series_equal(data["unit"], pd.Series([None, None]), check_names=False)
+    pd.testing.assert_series_equal(data["view_role"], pd.Series(["view_internal", "view_internal"]), check_names=False)
+    pd.testing.assert_series_equal(data["metadata"], pd.Series([{}, {}]), check_names=False)
+    pd.testing.assert_series_equal(data["id"], pd.Series([1, 2]), check_names=False)
+
+
+def test_direct_dp_creation_extensive(clean_db, sql_engine_data_source):
+    """
+    Tests directly creating a data point by inserting into the table
+
+    In contrast to the minimal variant, additional elements are populated
+    """
+
+    with sql_engine_data_source.connect() as con:
+        con.execute(sql.text("""
+            INSERT INTO data_points(name, device_id, location_code, data_provider, view_role, metadata) VALUES
+                ('name_0', 'device_0', 'nowhere special', 'crystal ball', 'view_internal', '{"hello": "json"}'),
+                ('name_1', NULL, 'here', 'common knowledge', 'view_public', '"hi everyone"');
+        """))
+
+        data = pd.read_sql("""
+            SELECT id, name, device_id, location_code, data_provider, unit, view_role, metadata
+                FROM data_points
+                ORDER BY name;
+        """, con)
+
+    assert data is not None
+    assert isinstance(data, pd.DataFrame)
+
+    pd.testing.assert_series_equal(data["name"], pd.Series(["name_0", "name_1"]), check_names=False)
+    pd.testing.assert_series_equal(data["device_id"], pd.Series(["device_0", None]), check_names=False)
+    pd.testing.assert_series_equal(data["location_code"], pd.Series(["nowhere special", "here"]), check_names=False)
+    pd.testing.assert_series_equal(data["unit"], pd.Series([None, None]), check_names=False)
+    pd.testing.assert_series_equal(data["view_role"], pd.Series(["view_internal", "view_public"]), check_names=False)
+    pd.testing.assert_series_equal(data["metadata"], pd.Series([{"hello": "json"}, "hi everyone"]), check_names=False)
+    pd.testing.assert_series_equal(data["id"], pd.Series([1, 2]), check_names=False)
+
+
+def test_role_based_dp_access(clean_db, sql_engine_data_source, sql_engine_private_vis, sql_engine_public_vis):
+    """Tests the view roles and their access privileges on the data points table"""
+
+    # Insert the data points
+    with sql_engine_data_source.connect() as con:
+        con.execute(sql.text("""
+            INSERT INTO data_points(name, device_id, location_code, data_provider, view_role) VALUES
+                ('name_0', 'device_0', 'nowhere special', 'crystal ball', 'view_internal'),
+                ('name_1', NULL, 'here', 'common knowledge', 'view_public');
+        """))
+        con.commit()
+
+    # Test the access right using the private user
+    with sql_engine_private_vis.connect() as con:
+        data = pd.read_sql("""SELECT name FROM data_points ORDER BY name;""", con)
+        pd.testing.assert_series_equal(data["name"], pd.Series(["name_0", "name_1"]), check_names=False)
+
+    # Test the access right using the public user
+    with sql_engine_public_vis.connect() as con:
+        data = pd.read_sql("""SELECT name FROM data_points ORDER BY name;""", con)
+        pd.testing.assert_series_equal(data["name"], pd.Series(["name_1"]), check_names=False)
